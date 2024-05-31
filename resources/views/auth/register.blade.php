02523ac4e535102b2e1061b399cc0df7c3259eaa
Franco Silva <franco_tigres18@hotmail.com>
2024-05-31 14:30:21 -0600

    feat(Controller, Views-Vacante):Creación de Controladro y rutas de vacantes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,6 +18,20 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <!-- Rol -->
+        <div class="mt-4">
+            <x-input-label for="rol" :value="__('¿Que tipo de cuenta deseas en devjobs?')" />
+
+            <select name="rol" id="rol"
+                class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full" required>
+                <option value="">--Selecciona un rol--</option>
+                <option value="1">Developer - Obtener Empleo</option>
+                <option value="2">Recruiter - Publicar Empleos</option>
+            </select>
+
+            <x-input-error :messages="$errors->get('rol')" class="mt-2" />
+        </div>
+
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Contraseña')" />
