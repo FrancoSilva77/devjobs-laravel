7ed01cf828c470dc8c93f6963274a92c6d4b7784
Franco Silva <franco_tigres18@hotmail.com>
2024-06-04 15:39:49 -0600

    feat(Categoria):Mostrar categorias en el formulario

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $this->call(SalarioSeeder::class);
+        $this->call(CategoriasSeeder::class);
     }
 }
