58eb983e25cb32272b2e988d496b4efd1409f76b
Franco Silva <franco_tigres18@hotmail.com>
2024-06-04 22:00:11 -0600

    feat(Vacante Form):Validación del Formulario

--- app/Livewire/CrearVacante.php
+++ app/Livewire/CrearVacante.php
@@ -8,11 +8,35 @@ use Livewire\Component;
 
 class CrearVacante extends Component
 {
+    public $titulo;
+    public $salario;
+    public $categoria;
+    public $empresa;
+    public $ultimo_dia;
+    public $descripcion;
+    public $imagen;
+
+    protected $rules = [
+        'titulo' => 'required|string',
+        'salario' => 'required',
+        'categoria' => 'required',
+        'empresa' => 'required',
+        'ultimo_dia' => 'required',
+        'descripcion' => 'required',
+        'imagen' => 'required'
+    ];
+
+    public function crearVacante() {
+        $datos = $this->validate();
+    }
+
     public function render()
     {
         // * Consultar DB
         $salarios = Salario::all();
         $categorias = Categoria::all();
+
+
 
         return view('livewire.crear-vacante', [
             'salarios' => $salarios,
