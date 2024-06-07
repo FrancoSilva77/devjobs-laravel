6b2228a0549f52bcdee7393d3966ca9b8809779d
Franco Silva <franco_tigres18@hotmail.com>
2024-06-07 08:18:18 -0600

    feat(Vacante):Guardar Vacante

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -57,7 +57,15 @@
 
     <div>
         <x-input-label for="imagen" :value="__('Imagen')" />
-        <x-text-input id="imagen" class="block mt-1 w-full" type="file" wire:model="imagen" />
+        <x-text-input id="imagen" class="block mt-1 w-full" type="file" wire:model="imagen" accept="image/*" />
+
+        <div class="my-5 w-80">
+            @if ($imagen)
+                Imagen:
+                <img src="{{ $imagen->temporaryUrl() }}" alt="">
+            @endif
+        </div>
+
         <x-input-error :messages="$errors->get('imagen')" class="mt-2" />
     </div>
 
