200853222e0bb2fd5f61dcc06992848a371ba0d6
Franco Silva <franco_tigres18@hotmail.com>
2024-06-08 12:10:42 -0600

    feat(vacantea):Listar vacantes

--- resources/views/vacantes/index.blade.php
+++ resources/views/vacantes/index.blade.php
@@ -12,11 +12,9 @@
                     {{ session('mensaje') }}
                 </div>
             @endif
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    Mis vacantes / Vacantes
-                </div>
-            </div>
+        
+            @livewire('mostrar-vacantes')
+            
         </div>
     </div>
 </x-app-layout>
